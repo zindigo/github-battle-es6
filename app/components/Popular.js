b4a0f5a448441a526e3235666cc54e3c5a676b2e
Barbara Verburg <barb@zenzillo.com>
2019-05-07 10:34:11 -0400

    Setup active navbar links

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			selectedLanguage: 'All'
+		}
+	}
+	updateLanguage(selectedLanguage) {
+		this.setState({
+			selectedLanguage
+		})
+	}
 	render() {
 		const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
 
@@ -8,7 +20,10 @@ export default class Popular extends React.Component {
 		    <ul className='flex-center'>
 		    	{languages.map((language) => (
 		    		<li key={language}>
-		    			<button className='btn-clear nav-link'>
+		    			<button
+		    				className='btn-clear nav-link'
+		    				style={language === this.state.selectedLanguage ? { color: 'rgb(187, 46, 31)' } : null}
+		    				onClick={() => this.updateLanguage(language)}>
 		    				{language}
 		    			</button>
 		    		</li>
