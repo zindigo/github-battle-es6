78f1508b07e55876a4f594e11978ecaf78c0b7c6
Barbara Verburg <barb@zenzillo.com>
2019-05-14 13:57:15 -0400

    Add query string to battle page

--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Popular from './components/Popular';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Popular from './components/Popular'
 import Battle from './components/Battle'
+import Results from './components/Results'
 import { ThemeProvider } from './contexts/theme'
 import Nav from './components/Nav'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -28,7 +29,8 @@ class App extends React.Component {
 				    <div className='container'>
 				    	<Nav />
 				    	<Route exact path='/' component={Popular} />
-				    	<Route path='/battle' component={Battle} />
+				    	<Route exact path='/battle' component={Battle} />
+				    	<Route path='/battle/results' component={Results} />
 				    </div>
 				</div>
 			  </ThemeProvider>
